980bfe34902b8761d2719954a2fd863e45483852
fangfangxu <472848022@qq.com>
2020-06-09 23:28:35 +0800

    SpringMVC第一天
    SpringMVC第一天

---
+++ spring-mvc/src/main/java/com/xufangfang/servlet/MyServlet.java
@@ -0,0 +1,21 @@
+package com.xufangfang.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+    private static final long serialVersionUID = -6546520536443387780L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("wyx最帅");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
